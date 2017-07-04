18b545bc2c6d6371da4bc291b5c39880b8e84be2
Aidan <mraidangrant@gmail.com>
2017-07-04 15:04:27 -0600

    Added tabs documentation.

--- src/demo/app/app.module.ts
+++ src/demo/app/app.module.ts
@@ -3,15 +3,24 @@ import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {AppComponent} from './app.component';
 import {VxComponentsModule} from './lib';
+import {TabsDemoComponent} from './tabs-demo/tabs-demo.component';
+import {Route, RouterModule} from '@angular/router';
+
+const routes: Route[] = [
+  { path: '', pathMatch: 'full', redirectTo: 'tabs'},
+  { path: 'tabs', component: TabsDemoComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabsDemoComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    VxComponentsModule
+    VxComponentsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
