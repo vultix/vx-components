d76442bd33102f90c89f7acfcb67793479a61ea0
Aidan <mraidangrant@gmail.com>
2017-12-03 09:58:09 -0700

    Started the documentation for the stepper.

--- src/demo/app/stepper-demo/stepper-demo.component.ts
+++ src/demo/app/stepper-demo/stepper-demo.component.ts
@@ -4,10 +4,47 @@ import {TitleService} from '../title.service';
 @Component({
   selector: 'vx-stepper-demo',
   templateUrl: './stepper-demo.component.html',
-  styleUrls: ['./stepper-demo.component.css']
+  styleUrls: ['./stepper-demo.component.scss']
 })
 export class StepperDemoComponent implements OnInit {
+  importLbl = `import {VxStepperModule} from 'vx-components';`;
 
+  example1Html = `<vx-checkbox #linear>Enable Linear Mode</vx-checkbox>
+<vx-stepper [linear]="linear.checked">
+  <vx-step label="Fill out your name" [stepControl]="nameInput">
+    <vx-input-wrapper>
+      <input vxInput placeholder="Enter Name" name="name" ngModel #nameInput="ngModel" required>
+    </vx-input-wrapper>
+    <div>
+      <button vx-button vxStepperNext>Next</button>
+    </div>
+  </vx-step>
+  <vx-step label="Fill out your address" [stepControl]="addressInput">
+    <vx-input-wrapper>
+      <input vxInput placeholder="Enter Your Address" name="address" ngModel #addressInput="ngModel" required>
+    </vx-input-wrapper>
+    <div>
+      <button vx-button vxStepperBack>Back</button>
+      <button vx-button vxStepperNext>Next</button>
+    </div>
+  </vx-step>
+  <vx-step label="Done">
+    You are finished!
+    <div>
+      <button vx-button vxStepperBack>Back</button>
+    </div>
+  </vx-step>
+</vx-stepper>`;
+
+  example1CSS = `button {
+  margin-top: 10px;
+}
+vx-input-wrapper {
+  width: 250px;
+}
+vx-stepper {
+  background: white;
+}`;
   constructor(titleService: TitleService) {
     titleService.title = 'Stepper';
   }
