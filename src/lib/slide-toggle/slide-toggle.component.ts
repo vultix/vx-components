56705e253789e3e953cd9576d225760084794b33
Vultix <mraidangrant@gmail.com>
2018-05-14 16:02:45 -0600

    Made the slide toggle actually slide with your finger / mouse.

--- src/lib/slide-toggle/slide-toggle.component.ts
+++ src/lib/slide-toggle/slide-toggle.component.ts
@@ -1,6 +1,9 @@
-import {Component, EventEmitter, forwardRef, HostBinding, HostListener, Input, Output} from '@angular/core';
+import {Component, ElementRef, EventEmitter, forwardRef, HostBinding, HostListener, Input, Output, ViewChild} from '@angular/core';
 import {ControlValueAccessor, NG_VALUE_ACCESSOR} from '@angular/forms';
 import {coerceBooleanProperty} from '../shared/util';
+
+const MAX_HANDLE_LEFT = 13;
+const MIN_HANDLE_LEFT = -3;
 
 @Component({
   selector: 'vx-slide-toggle',
@@ -15,8 +18,27 @@ import {coerceBooleanProperty} from '../shared/util';
   ]
 })
 export class VxSlideToggleComponent implements ControlValueAccessor {
+  /** Whether the checkbox is disabled. */
+  @HostBinding('class.disabled')
+  @Input() disabled = false;
+  /** Name value will be applied to the input element if present */
+  @Input() name: string;
+  /** Emitted when the checkbox's `checked` value changes. */
+  @Output() checkedChange = new EventEmitter<boolean>();
+
+  handleLeft = -3;
+  transitionHandle = true;
+
+  private touchX: number;
+  private touchDown = false;
+  private didMove = false;
+
+  /** NgModel support */
+  private _onChangeFn = (v: boolean) => v;
+  private _onTouchedFn = () => {
+  };
+
   private _tabIndex = 0;
-  private _checked = false;
 
   /** The checkbox's tabIndex */
   @HostBinding('attr.tabIndex')
@@ -29,12 +51,7 @@ export class VxSlideToggleComponent implements ControlValueAccessor {
     this._tabIndex = tabIndex;
   };
 
-  /** Whether the checkbox is disabled. */
-  @HostBinding('class.disabled')
-  @Input() disabled = false;
-
-  /** Name value will be applied to the input element if present */
-  @Input() name: string;
+  private _checked = false;
 
   /** Whether the checkbox is checked */
   @HostBinding('class.checked')
@@ -48,28 +65,24 @@ export class VxSlideToggleComponent implements ControlValueAccessor {
     this._onChangeFn(checked);
     this._onTouchedFn();
     this._checked = checked;
+    this.checkedChange.emit(checked);
+    this.updateHandleLeft();
   };
-
-  /** Emitted when the checkbox's `checked` value changes. */
-  @Output() checkedChange = new EventEmitter<boolean>();
 
   /** Toggles the `checked` state of the checkbox. */
   @HostListener('click')
   @HostListener('keydown.space')
   @HostListener('keydown.enter')
   public toggle(): boolean {
+    if (this.didMove) {
+      this.didMove = false;
+      return;
+    }
+
     this.checked = !this.checked;
-    this.checkedChange.emit(this.checked);
-    this._onChangeFn(this.checked);
-    this._onTouchedFn();
 
     return false;
   }
-
-  /** NgModel support */
-  private _onChangeFn = (v: boolean) => v;
-  private _onTouchedFn = () => {
-  };
 
   writeValue(checked: boolean): void {
     if (checked === true || checked === false && this._checked !== checked) {
@@ -85,5 +98,63 @@ export class VxSlideToggleComponent implements ControlValueAccessor {
   registerOnTouched(fn: any): void {
     this._onTouchedFn = fn;
   }
+
+  handleTouchStart(event: MouseEvent | TouchEvent): void {
+    this.touchX = getTouchPos(event);
+    this.touchDown = true;
+    this.didMove = false;
+  }
+
+  handleTouchMove(event: MouseEvent | TouchEvent): void {
+    if (this.touchDown) {
+      const newTouchX = getTouchPos(event);
+
+      this.handleLeft += newTouchX - this.touchX;
+
+      if (this.handleLeft < MIN_HANDLE_LEFT) {
+        this.handleLeft = MIN_HANDLE_LEFT
+      } else if (this.handleLeft > MAX_HANDLE_LEFT) {
+        this.handleLeft = MAX_HANDLE_LEFT;
+      }
+
+      this.touchX = newTouchX;
+      this.transitionHandle = false;
+      this.didMove = true;
+
+      event.preventDefault();
+    }
+  }
+
+
+  @HostListener('window:mouseup')
+  @HostListener('window:touchend')
+  handleTouchEnd(): void {
+    this.touchDown = false;
+    this.transitionHandle = true;
+
+    const dragArea = MAX_HANDLE_LEFT - MIN_HANDLE_LEFT;
+    const dragPos = this.handleLeft - MIN_HANDLE_LEFT;
+
+    if (dragPos >= (dragArea / 2) && !this.checked) {
+      this.checked = true;
+    } else if (dragPos < (dragArea / 2) && this.checked) {
+      this.checked = false;
+    } else {
+      this.updateHandleLeft();
+    }
+
+  }
+
+  private updateHandleLeft(): void {
+    this.handleLeft = this.checked ? MAX_HANDLE_LEFT : MIN_HANDLE_LEFT;
+  }
 }
 
+
+function getTouchPos(event: MouseEvent | TouchEvent): number {
+  if (event instanceof TouchEvent) {
+    return event.touches[0].clientX;
+  } else {
+    return event.clientX;
+  }
+}
