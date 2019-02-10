d80cbe6e4d11ebf17ac294bfa1baa34946833cc7
Vultix <mraidangrant@gmail.com>
2019-02-10 10:53:22 -0700

    Hooked into the build process to build the scss.

---
+++ projects/vx-ns-components/ng-packagr.transformers.ts
@@ -0,0 +1,27 @@
+import { NgPackagerHooks, NgPackagerHooksContext } from 'ng-cli-packagr-tasks';
+import { join } from 'path';
+import { Bundler } from 'scss-bundle';
+import * as fs from 'fs';
+
+import { promisify } from 'util';
+
+const writeFile = promisify(fs.writeFile);
+
+const hooks: NgPackagerHooks = {
+  writePackage: {
+    before: async taskContext => {
+      const globalContext = taskContext.context();
+      const workspaceRoot = globalContext.builderContext.workspace.root;
+      // const srcRoot = globalContext.builderConfig.sourceRoot;
+
+      // Builds the scss
+      const bundler = new Bundler(undefined, workspaceRoot);
+
+      const result = await bundler.Bundle('./projects/vx-ns-components/src/theming.scss');
+
+      await writeFile(join(workspaceRoot, 'dist/vx-ns-components/theming.scss'), result.bundledContent);
+    }
+  }
+};
+module.exports = hooks;
+// return hooks;
